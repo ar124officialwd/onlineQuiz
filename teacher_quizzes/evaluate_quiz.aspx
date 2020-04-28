9683abe2223b11a8d705e04c14c64a5923259c19
Ahmad Raza <ar124official2019@gmail.com>
2020-04-28 20:51:51 +0100

    Added web form to evaluate a submission to a quiz

---
+++ teacher_quizzes/evaluate_quiz.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/teacher_quizzes/teacher_quizzes.master" AutoEventWireup="true" CodeBehind="evaluate_quiz.aspx.cs" Inherits="onlineQuiz_bsef17m35.teacher_quizzes.evaluate_quiz" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="teacherQuizHead" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="quizes" runat="server">
+  <div class="alert alert-success m-1 p-2" runat="server"
+    id="resultIssued" visible="false">
+    Result has been issued! Click <a href="submissions.aspx">here</a>
+    to go back to submissions.
+  </div>
+
+  <div class="p-2" runat="server" id="mainView">
+    <div class="alert alert-dark p-2">
+      <b runat="server" id="quizTitle"></b>
+    </div>
+
+    <div class="p-2 d-flex flex-column">
+      <div>
+        <b>Student Name: </b><span runat="server" id="studentName"></span>
+      </div>
+      <div>
+        <b>Student ID: </b><span runat="server" id="studentEmail"></span>
+      </div>
+    </div>
+
+    <div runat="server" id="evaluationView">
+      <asp:Repeater runat="server" ID="questionRepeater">
+        <ItemTemplate>
+          <div class="m-1 asp-panel p-2">
+            <div>
+              <b>Question: </b><span><%# Eval("question") %></span>
+            </div>
+
+            <div>
+              <b>Question Type:</b>
+              <span class="p-2"><%# Eval("type") %></span>
+            </div>
+
+            <div>
+              <b>Max Grades:</b>
+              <span class="p-2"><%# Eval("marks") %></span>
+            </div>
+
+            <div>
+              <b>Given Answer(s):</b>
+              <div class="m-1 py-1 px-2 border-1 border-left border-primary"><%# Eval("answer") %></div>
+              <asp:Repeater ItemType="System.string" runat="server" ID="answers">
+                <ItemTemplate>
+                  <div class="py-1 px-2 border-1 border-left border-primary"><%# Item %></div>
+                </ItemTemplate>
+              </asp:Repeater>
+            </div>
+
+            <div>
+              <b>Grades</b>
+              <asp:TextBox runat="server" CssClass="form-control m-1" TextMode="number"
+                ID="questionGrades" Text="0" />
+
+              <asp:RangeValidator runat="server" ControlToValidate="questionGrades"
+                Type="Integer" MinimumValue="0" MaximumValue='<%# Eval("marks") %>'
+                CssClass="text-danger" ErrorMessage="Grade cannot excceed max grades!">
+              </asp:RangeValidator>
+            </div>
+          </div>
+        </ItemTemplate>
+      </asp:Repeater>
+
+      <div class="m-1 d-flex flex-row">
+        <button class="btn btn-primary" runat="server" id="reviewResult"
+          onserverclick="reviewResult_ServerClick">
+          Preview Result</button>
+      </div>
+    </div>
+
+    <div runat="server" id="resultPreview" class="p-2" visible="false">
+      <div class="alert alert-danger" runat="server" id="issueResultError"
+        visible="false"></div>
+
+      <div>
+        <b>Total Marks: </b><span runat="server" id="totalMarks"></span>
+      </div>
+
+      <div>
+        <b>Total Marks: </b><span runat="server" id="passingMarks"></span>
+      </div>
+
+      <div>
+        <b>Marks Obtained: </b><span runat="server" id="marksObtained"></span>
+      </div>
+
+      <div>
+        <b>Status: </b><span runat="server" id="status"></span>
+      </div>
+
+      <div class="m-1 d-flex flex-row">
+        <button class="btn btn-primary mr-1" runat="server" id="reEvaluateResult"
+          onserverclick="reEvaluateResult_ServerClick">
+          Re-Evaluate Result</button>
+        <button class="btn btn-primary" runat="server" id="issueResult"
+          onserverclick="issueResult_ServerClick">
+          Issue Result</button>
+      </div>
+    </div>
+  </div>
+</asp:Content>
---
+++ teacher_quizzes/evaluate_quiz.aspx.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace onlineQuiz_bsef17m35.teacher_quizzes
+{
+  public partial class evaluate_quiz : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      if (IsPostBack)
+      {
+        return;
+      }
+
+      var teacherId = Int32.Parse(Session["userId"].ToString());
+      var quizId = -1;
+      var studentId = -1;
+
+      try
+      {
+        quizId = Int32.Parse(Request.QueryString["quizId"]);
+        studentId = Int32.Parse(Request.QueryString["studentId"]);
+        var database = new DatabaseEntities();
+
+        var dbSubmission = database.getSubmission(teacherId, quizId, studentId).Single();
+        var submissionJSON = dbSubmission.content;
+        var submission = JsonConvert.DeserializeObject<LocalQuiz>(submissionJSON);
+
+        quizTitle.InnerText = submission.title;
+        studentName.InnerText = dbSubmission.studentName;
+        studentEmail.InnerText = dbSubmission.studentEmail;
+
+        ViewState["questions"] = JsonConvert.SerializeObject(submission.questions);
+        ViewState["totalMarks"] = submission.totalMarks;
+        ViewState["passingMarks"] = submission.passingMarks;
+        ViewState["quizId"] = submission.id;
+        ViewState["teacherId"] = submission.teacherId;
+        ViewState["studentId"] = studentId;
+
+        questionRepeater.DataSource = submission.questions;
+        questionRepeater.DataBind();
+
+        foreach (RepeaterItem item in questionRepeater.Items)
+        {
+          var q = submission.questions[item.ItemIndex];
+          var gradeControl = (TextBox)item.FindControl("questionGrades");
+
+          if (q.type == "Multiple Choice")
+          {
+            var correctOption = database.QuestionOption.Where(qo =>
+              qo.questionId == q.id && qo.teacherId == teacherId &&
+              qo.quizId == quizId && qo.valid == true).Single();
+            var grade = (correctOption.value == q.answer) ? q.marks : 0;
+
+            if (gradeControl != null)
+            {
+              gradeControl.Text = grade.ToString();
+              gradeControl.Enabled = false;
+            }
+
+            continue;
+          }
+
+          if (q.type == "Checkboxes")
+          {
+            /* show answers */
+            var answers = (Repeater)item.FindControl("answers");
+            if (answers != null)
+            {
+              answers.DataSource = q.answers;
+              answers.DataBind();
+            }
+
+            /* set grade */
+            var correct = true;
+            var correctOptions = database.QuestionOption.Where(qo =>
+              qo.questionId == q.id && qo.teacherId == teacherId &&
+              qo.quizId == quizId && qo.valid == true).ToList();
+
+            if (correctOptions.Count() != q.answers.Count())
+            {
+              correct = false;
+            } else
+            {
+              foreach (var o in correctOptions)
+              {
+                if (!q.answers.ToList().Contains(o.value))
+                {
+                  correct = false;
+                  break;
+                }
+              }
+            }
+
+            int grade = correct ? q.marks : 0;
+            if (gradeControl != null)
+            {
+              gradeControl.Text = grade.ToString();
+              gradeControl.Enabled = false;
+            }
+          }
+        }
+      }
+      catch (Exception err)
+      {
+        Response.Redirect("/teacher_quizzes/all_quizzes.aspx");
+      }
+    }
+
+    protected void issueResult_ServerClick(object sender, EventArgs e)
+    {
+      evaluate();
+      var _obtainedMarks = Int32.Parse(marksObtained.InnerText);
+      var _status = status.InnerText;
+      var database = new DatabaseEntities();
+
+      try
+      {
+        var result = new Result();
+        result.quizId = Int32.Parse(ViewState["quizId"].ToString());
+        result.teacherId = Int32.Parse(ViewState["teacherId"].ToString());
+        result.studentId = Int32.Parse(ViewState["studentId"].ToString());
+        result.obtainedMarks = _obtainedMarks;
+        result.status = _status;
+        result.questions = ViewState["questions"].ToString();
+
+        database.Result.Add(result);
+        database.SaveChanges();
+
+        resultIssued.Visible = true;
+        mainView.Visible = false;
+      } catch(Exception err)
+      {
+        issueResultError.InnerText = "Something went wrong! Unabled to issue result";
+        issueResultError.Visible = true;
+      }
+    }
+
+    protected void reviewResult_ServerClick(object sender, EventArgs e)
+    {
+      evaluate();
+      resultPreview.Visible = true;
+      evaluationView.Visible = false;
+    }
+
+    protected void reEvaluateResult_ServerClick(object sender, EventArgs e)
+    {
+      resultPreview.Visible = false;
+      evaluationView.Visible = true;
+    }
+
+    protected void evaluate()
+    {
+      var _obtainedMarks = 0;
+      var _totalMarks = Int32.Parse(ViewState["totalMarks"].ToString());
+      var _passingMarks = Int32.Parse(ViewState["passingMarks"].ToString());
+
+      foreach(RepeaterItem item in questionRepeater.Items)
+      {
+        var grade = (TextBox)item.FindControl("questionGrades");
+        if (grade != null)
+        {
+          _obtainedMarks += Int32.Parse(grade.Text);
+        }
+
+        var questions = JsonConvert
+          .DeserializeObject<LocalQuestion[]>(ViewState["questions"].ToString());
+        questions[item.ItemIndex].obtainedMarks = Int32.Parse(grade.Text);
+        ViewState["questions"] = JsonConvert.SerializeObject(questions);
+      }
+
+      totalMarks.InnerText = _totalMarks.ToString();
+      passingMarks.InnerText = _passingMarks.ToString();
+      marksObtained.InnerText = _obtainedMarks.ToString();
+
+      if (_obtainedMarks >= _passingMarks)
+      {
+        status.InnerText = "Passed";
+        status.Attributes["class"] = "text-success";
+      } else
+      {
+        status.InnerText = "Failed";
+        status.Attributes["class"] = "text-danger";
+      }
+    }
+  }
+}
